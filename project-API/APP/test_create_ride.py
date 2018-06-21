d1c791c2fb9e8b81ac0618255552c827dc92dbb3
dygn <chardick89@gmail.com>
2018-06-21 16:12:05 +0300

    [ft-#158524300] added a new ride test

--- project-API/APP/test_create_ride.py
+++ project-API/APP/test_create_ride.py
@@ -1,7 +1,7 @@
 from APP import app
 import json
 
-def test_createride():
+def test_get_all_rides():
     """test that the API can fetch all rides available"""
     client = app.test_client()
     res = client.get('/api/v1/rides')
