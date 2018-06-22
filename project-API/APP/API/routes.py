7a44c25f2f48060f3abf15919c5e7ee9c1b79705
dygn <chardick89@gmail.com>
2018-06-22 13:29:24 +0300

    [ft-#158524423] merged project-API from ft-test-create-ride API

---
+++ project-API/APP/API/routes.py
@@ -0,0 +1,46 @@
+from APP.API import Bluep
+
+import json
+
+from flask import request, jsonify, make_response
+
+
+def ride_resource():
+    """implement ride resource as a list of dictionary"""
+    rides = [{"id":1, "destination":"Nairobi", "departure_point":"Eldoret", "fare":800, "driver":{"username":"Jesse", "gender":"male"},
+ "passengers":{"usernames":["jane", "jack", "frank"],"number":3}, "stop_over":["Nakuru","Limuru"]}, {"id":2, "destination":"Mombasa", "departure_point":"Nairobi", "fare":1200, "driver":{"username":"Milac", "gender":"female"},
+"passengers":{"usernames":["jeff", "steve"],"number":2}, "stop_over":["Mtito Andei"]}] 
+    return rides
+
+@Bluep.route('/rides', methods=['GET'])
+def get_rides():
+    """get all ride"""
+    return make_response(jsonify(ride_resource()))
+
+
+@Bluep.route('/rides', methods=['POST'])
+def create_ride(): 
+    data = request.get_json()
+    id = data['id']
+    destination = data['destination']
+    departure_point = data['departure_point']
+    fare = data['fare']
+    driver = data['driver']
+    return make_response(jsonify({"results":"success",
+                                 "status": "ok",
+                                 "id": id , 
+                                 "destination": destination, 
+                                 "fare": fare,
+                                 "driver":driver
+
+                                }), 201)
+
+
+@Bluep.route('/rides/<int:id>/driver', methods=['GET'])
+def get_driver(id):
+    pass
+
+@Bluep.route('/rides/<int:id>', methods=['POST'])
+def request_user(id):
+    pass
+
---
+++ project-API/APP/test_create_ride.py
@@ -0,0 +1,26 @@
+from APP import app
+import json
+
+def test_create_ride():
+    """test that the API can fetch all rides available"""
+    client = app.test_client()
+    dt={"id": 6, "destination":"san francisco", "departure_point":"miami", "fare":1000, "driver":{"username":"amos"}}
+    response = client.post('/api/v1/rides', data = json.dumps(dt) , content_type = 'application/json' )
+    result = json.loads(response.data)
+    assert response.status_code == 201
+    assert 'destination' in response.get_data(as_text=True)
+    assert result['status'] == 'ok'
+    assert result['destination'] =='san francisco'
+    #assert isinstance(dat, list) == True
+    #assert len(dat) == 3
+    #assertEqual(data['count'], 0)
+    #assertIsInstance(data['count'], int)
+    #assertEqual(data['previous'], None)
+    #assertEqual(data['next'], None)
+
+#def test_invalid_JSON(self):
+    #"""Test status code 405 from improper JSON on post to raw"""
+    #client = app.test_client()
+    #res = client.post('/api/v1/rides', data="This isn't a json... it's a string!")
+    
+    #assert res.status_code == 405
