4b864e54fa4a0fcbafea2d10782b4a7157c85216
Dickens <40241345+Dygn@users.noreply.github.com>
2018-06-23 12:08:33 +0300

    update test

--- project-API/APP/test_create_ride.py
+++ project-API/APP/test_create_ride.py
@@ -6,7 +6,7 @@ def test_create_ride():
     client = app.test_client()
     dt={"id": 6, "destination":"san francisco", "departure_point":"miami", "fare":1000, "driver":{"username":"amos"}}
     response = client.post('/api/v1/rides', data = json.dumps(dt) , content_type = 'application/json' )
-    result = json.loads(response.data)
+    result = json.loads(response.data.decode())
     assert response.status_code == 201
     assert 'destination' in response.get_data(as_text=True)
     assert result['status'] == 'ok'
