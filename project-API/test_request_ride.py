26c6fb2594b7a0135502422526aa71d616ab89e9
Dickens <40241345+Dygn@users.noreply.github.com>
2018-06-24 00:10:43 +0300

    update test

--- project-API/test_request_ride.py
+++ project-API/test_request_ride.py
@@ -6,7 +6,7 @@ def test_create_ride():
     client = app.test_client()
     dt={"username":"dickens", "state":"requesting"}
     response = client.put('/api/v1/rides/1', data = json.dumps(dt) , content_type = 'application/json' )
-    result = json.loads(response.data)
+    result = json.loads(response.data.decode())
     assert response.status_code == 201
     #assertTrue(res.content_type == 'application/json')
     #self.assertTrue(data['status'] == 'success')
