0253319afe4bea499c0e5d97a30f56a4427845de
Dickens <40241345+Dygn@users.noreply.github.com>
2018-06-23 12:03:11 +0300

    update tests

--- project-API/test_login.py
+++ project-API/test_login.py
@@ -6,13 +6,13 @@ client = app.test_client()
 
 def test_login():
         response = client.get('/api/v1/login')
-        result = json.loads(response.data)
+        result = json.loads(response.data.decode())
         assert result["message"] == "logged in"
         assert response.status_code == 200
 
 def test_register():
         response = client.post('/api/v1/register', data = json.dumps(dt) , content_type = 'application/json')
-        result = json.loads(response.data)
+        result = json.loads(response.data.decode())
         assert result["username"] == "luqman"
         assert result["email"] == "luqman@yahoo.com"
         assert result["password"]== "123abc"
